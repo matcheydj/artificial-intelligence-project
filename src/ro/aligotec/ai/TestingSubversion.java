a071274f2643e2a5f46bf3e5f93eb151247e5ba4
TECIGO <TECIGO@gmail.com>
2011-05-21 14:35:33 +0000

    First revision...

---
+++ src/ro/aligotec/ai/TestingSubversion.java
@@ -0,0 +1,13 @@
+package ro.aligotec.ai;
+
+public class TestingSubversion {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
